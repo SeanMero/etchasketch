ad3299583550e31b39096eb3b7b0d142476e3534
SeanMero <MeroFromVero@gmail.com>
2024-03-20 16:32:22 +0800

    Add javascript and styles to generate 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-//create for loop to put four flex:column divs inside containerOuter
+//put four flex:column divs inside containerOuter
+const containerOuter = document.getElementById("containerOuter");
+for (let i = 1; i < 17; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.className = "columnDiv";
+    containerOuter.appendChild(newDiv);
+};
 
 //create four divs inside each column
+const columnDivsCollection = document.getElementsByClassName("columnDiv");
+const columnDivs = Array.from(columnDivsCollection);
+
+columnDivs.forEach(element => {
+    for (let i = 1; i < 17; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "rowDiv";
+        element.appendChild(newDiv);
+    };
+});
