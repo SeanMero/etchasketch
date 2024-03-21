611a778ab52c38b6238e011258441c3ef7627ab3
SeanMero <MeroFromVero@gmail.com>
2024-03-21 16:33:23 +0800

    Add function to replace grid with a new one of user-defined size and make colors random

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//put four flex:column divs inside containerOuter
+//put 16 flex:column divs inside containerOuter
 const containerOuter = document.getElementById("containerOuter");
 for (let i = 1; i < 17; i++) {
     const newDiv = document.createElement("div");
@@ -6,9 +6,9 @@ for (let i = 1; i < 17; i++) {
     containerOuter.appendChild(newDiv);
 };
 
-//create four divs inside each column
-const columnDivsCollection = document.getElementsByClassName("columnDiv");
-const columnDivs = Array.from(columnDivsCollection);
+//create 16 divs inside each column
+let columnDivsCollection = document.getElementsByClassName("columnDiv");
+let columnDivs = Array.from(columnDivsCollection);
 
 columnDivs.forEach(element => {
     for (let i = 1; i < 17; i++) {
@@ -18,13 +18,57 @@ columnDivs.forEach(element => {
     };
 });
 
-//change rowDiv color on hover
-const rowDivCollection = document.getElementsByClassName("rowDiv");
-const rowDivs = Array.from(rowDivCollection);
+//change rowDiv to a random color on hover
+let rowDivCollection = document.getElementsByClassName("rowDiv");
+let rowDivs = Array.from(rowDivCollection);
+//let randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 rowDivs.forEach(element => {
     element.addEventListener("mouseenter", function(e) {
         // [debug] console.log("Color changed!")
-        e.target.setAttribute("style", "background: blue;");
+        e.target.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
     });
 });
+
+//get user's choice for side lengths
+let userChose = 0;
+
+const choice = document.getElementById("choice");
+let userChoice = function() {
+    userChose = prompt("Please enter the size");
+    userChose = Number(userChose);
+    return userChose;
+};
+
+choice.addEventListener("click", userChoice);
+
+//reset Sketch according to user's choice
+let baleet = function() {
+    rowDivs.forEach(element => {
+        element.remove();
+     });
+    columnDivs.forEach(element => {
+        element.remove();
+    });
+    for (let i = 1; i <= userChose; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.className = "columnDiv";
+        containerOuter.appendChild(newDiv);
+    };
+    columnDivs = Array.from(columnDivsCollection);
+    columnDivs.forEach(element => {
+        for (let i = 1; i <= userChose; i++) {
+            const newRow = document.createElement("div");
+            newRow.className = "rowDiv";
+            element.appendChild(newRow);
+        };
+    });
+    rowDivs = Array.from(rowDivCollection);
+    rowDivs.forEach(element => {
+        element.addEventListener("mouseenter", function(e) {
+            e.target.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);;
+        });
+    });
+};
+
+choice.addEventListener("click", baleet)
